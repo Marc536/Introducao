9d6212f8d8b61e93af3d4c8f64f066e62a3aa76d
Marc536 <79657618+Marc536@users.noreply.github.com>
2021-07-06 20:56:23 -0300

    Update "Hello World" code with new to-do items

--- app.py
+++ app.py
@@ -1,15 +1,34 @@
 import web
+import sys
+from web import form
 
-urls = (
-    '/(.*)', 'hello'
-)
+render = web.template.render('templates/')
+
+urls = ('/', 'hello')
 app = web.application(urls, globals())
 
+myform = form.Form(
+    form.Textbox("Input")
+)
+
 class hello:
-    def GET(self, name):
-        if not name:
-            name = 'World'
-        return 'Hello, ' + name + '!'
+    def __init__(self):
+        self.model = ""
+        self.stdout_console = sys.stdout
+
+    def GET(self):
+        form = myform()
+        return render.World(form)
+
+    def POST(self):
+        form = myform()
+        if not form.validates(): 
+            return render.World(form)
+        else:
+            self.model = form['Input'].value
+            self.stdout_console.write(form['Input'].value + "\n")
+            return render.World(form)
 
 if __name__ == "__main__":
+    web.internalerror = web.debugerror
     app.run()
